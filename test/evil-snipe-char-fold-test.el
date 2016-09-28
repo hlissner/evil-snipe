af03c7c216b9308e4698595f00d9d43dff78956b
Henrik Lissner <henrik@lissner.net>
2016-09-28 14:16:49 +0200

    Add emacs version guard to char-fold unit test

--- test/evil-snipe-char-fold-test.el
+++ test/evil-snipe-char-fold-test.el
@@ -1,17 +1,19 @@
 ;;; evil-snipe-char-fold-test.el
 
-(ert-deftest evil-snipe-char-fold-test ()
-  (with! "And ªnd ànd ånd ānd ănd ąnd ǎnd ȁnd and"
-    (let ((evil-snipe-char-fold t))
-      (should (from! (point-min) (evil-snipe-f 1 [?a])
-                     (looking-at-p "ª")))
-      (should (from! (point-min) (evil-snipe-f 2 [?a])
-                     (looking-at-p "à")))
+(when (or (> emacs-major-version 25)
+          (and (= emacs-major-version 25) (>= emacs-minor-version 1)))
+  (ert-deftest evil-snipe-char-fold-test ()
+    (with! "And ªnd ànd ånd ānd ănd ąnd ǎnd ȁnd and"
+      (let ((evil-snipe-char-fold t))
+        (should (from! (point-min) (evil-snipe-f 1 [?a])
+                       (looking-at-p "ª")))
+        (should (from! (point-min) (evil-snipe-f 2 [?a])
+                       (looking-at-p "à")))
 
-      (should (from! (point-min) (evil-snipe-s 1 [?\  ?a])
-                     (looking-at-p " ª")))
-      (should (from! (point-min) (evil-snipe-s 2 [?\  ?a])
-                     (looking-at-p " à"))))))
+        (should (from! (point-min) (evil-snipe-s 1 [?\  ?a])
+                       (looking-at-p " ª")))
+        (should (from! (point-min) (evil-snipe-s 2 [?\  ?a])
+                       (looking-at-p " à")))))))
 
 (provide 'evil-snipe-char-fold-test)
 ;;; evil-snipe-char-fold-test.el ends here
