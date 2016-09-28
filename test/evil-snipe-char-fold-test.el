19b639f3b4a3ee482a5a4f0264aff720cfee9949
Henrik Lissner <henrik@lissner.net>
2016-09-28 14:08:14 +0200

    Add char-fold unit test

---
+++ test/evil-snipe-char-fold-test.el
@@ -0,0 +1,17 @@
+;;; evil-snipe-char-fold-test.el
+
+(ert-deftest evil-snipe-char-fold-test ()
+  (with! "And ªnd ànd ånd ānd ănd ąnd ǎnd ȁnd and"
+    (let ((evil-snipe-char-fold t))
+      (should (from! (point-min) (evil-snipe-f 1 [?a])
+                     (looking-at-p "ª")))
+      (should (from! (point-min) (evil-snipe-f 2 [?a])
+                     (looking-at-p "à")))
+
+      (should (from! (point-min) (evil-snipe-s 1 [?\  ?a])
+                     (looking-at-p " ª")))
+      (should (from! (point-min) (evil-snipe-s 2 [?\  ?a])
+                     (looking-at-p " à"))))))
+
+(provide 'evil-snipe-char-fold-test)
+;;; evil-snipe-char-fold-test.el ends here
