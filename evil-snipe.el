c3e5e26c14f4a47daedb4fc48e1b0fb993aa0999
Henrik Lissner <henrik@lissner.net>
2017-04-05 19:44:47 -0400

    v2.0.7 bump

--- evil-snipe.el
+++ evil-snipe.el
@@ -5,8 +5,8 @@
 ;; Author: Henrik Lissner <http://github/hlissner>
 ;; Maintainer: Henrik Lissner <henrik@lissner.net>
 ;; Created: December 5, 2014
-;; Modified: March 28, 2017
-;; Version: 2.0.6
+;; Modified: April 05, 2017
+;; Version: 2.0.7
 ;; Keywords: emulation, vim, evil, sneak, seek
 ;; Homepage: https://github.com/hlissner/evil-snipe
 ;; Package-Requires: ((evil "1.2.12") (cl-lib "0.5"))
